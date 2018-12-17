20a67fd99689894607334066da4a4baaff1b36a6
metajet98 <bac.nguyenxuan98@gmail.com>
2018-12-17 15:02:40 +0700

    Public demo

---
+++ WpfApp1/DAL/SinhVienDAL.cs
@@ -0,0 +1,42 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class SinhVienDAL    
+
+    {
+        private static ObservableCollection<SinhVien> listSinhVien = new ObservableCollection<SinhVien>() { new SinhVien() { Id = 1, Ten = "Long" }, new SinhVien() { Id = 2, Ten = "Hoang" } ,new SinhVien() { Id = 3, Ten = "Phan" }, new SinhVien() { Id = 4, Ten = "Trung" }};
+        
+        public static ObservableCollection<SinhVien> getListSinhVienFromDatabase()
+        {
+           
+            
+            return listSinhVien;
+        }
+        public static void DeleteSinhVien(SinhVien sinhVien)
+        {
+            listSinhVien.Remove(sinhVien);
+        }
+        public static void AddSinhVien(SinhVien sinhVien)
+        {
+            listSinhVien.Add(sinhVien);
+        }
+        public static void ModifySinhVien(SinhVien sinhVien)
+        {
+            foreach (var sv in listSinhVien)
+            {
+                if(sv.Id==sinhVien.Id)
+                {
+                    sv.Ten = sinhVien.Ten;
+                }
+            }
+        }
+
+    }
+}
---
+++ WpfApp1/ViewModel/SinhVienViewModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using DAL;
+using Model;
+
+namespace ViewModel
+{
+    public class SinhVienViewModel
+    {
+        private ObservableCollection<SinhVien> listSinhVien;
+
+        public ObservableCollection<SinhVien> ListSinhVien
+        {
+            get { return listSinhVien; }
+            set { listSinhVien = value; }
+        }
+        public SinhVienViewModel()
+        {
+            updateSinhVien();
+            ModifyCommand = new RelayCommand<UIElementCollection>((p) => p != null, (p) => {
+                int id = 0;
+                string ten = "";
+                bool isIDInt = false;
+
+                foreach (var item in p)
+                {
+                    TextBox a = item as TextBox;
+                    if (a == null)
+                        continue;
+                    switch (a.Name)
+                    {
+                        case "txbID":
+                            isIDInt = Int32.TryParse(a.Text, out id);
+                            break;
+                        case "txbTen":
+                            ten = a.Text;
+                            break;
+
+                    }
+
+                }
+                if (!isIDInt || string.IsNullOrEmpty(ten))
+                {
+                    return;
+                }
+                SinhVien b = new SinhVien() { Id = id, Ten = ten };
+                SinhVienDAL.ModifySinhVien(b);
+                updateSinhVien();
+            });
+            DeleteCommand = new RelayCommand<object>((p) => p != null, (p) => {
+               SinhVienDAL.DeleteSinhVien(p as SinhVien);
+                updateSinhVien();
+            });
+            AddCommand = new RelayCommand<UIElementCollection>((p) => p != null, (p) => {
+                int id = 0;
+                string ten = "";
+                bool isIDInt = false;
+
+                foreach (var item in p)
+                {
+                    TextBox a = item as TextBox;
+                    if (a == null)
+                        continue;
+                    switch(a.Name)
+                    {
+                        case "txbID":
+                            isIDInt = Int32.TryParse(a.Text, out id);
+                            break;
+                        case "txbTen":
+                            ten = a.Text;
+                            break;
+
+                    }
+                    
+                }
+                if (!isIDInt || string.IsNullOrEmpty(ten))
+                {
+                    return;
+                }
+                SinhVien b = new SinhVien() { Id = id, Ten = ten };
+                SinhVienDAL.AddSinhVien(b);
+                updateSinhVien();
+            });
+
+
+        }
+        public ICommand DeleteCommand { get; set; }
+
+        public ICommand AddCommand { get; set; }
+
+        public ICommand ModifyCommand { get; set; }
+        void updateSinhVien()
+        {
+            listSinhVien = SinhVienDAL.getListSinhVienFromDatabase();
+
+        }
+
+    }
+
+}
